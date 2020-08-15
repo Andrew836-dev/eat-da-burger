a5d2512b49310245e056548e5e2132bb77790dcd
Andrew Gray <andrew.aj.gray@gmail.com>
2020-08-15 16:12:36 +0930

    modified backend for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 const mysql = require("mysql");
+var connection;
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: "3306",
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: "3306",
 
-    user: "root",
-    password: "abc121",
-    database: "burgers_db"
-});
-
+        user: "root",
+        password: "abc121",
+        database: "burgers_db"
+    });
+}
 connection.connect(function (err) {
     if (err) console.log(err.stack);
     else console.log("Connected on thread :", connection.threadId);
